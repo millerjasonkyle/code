266891c51cc89c0e011254b7f633cc06a9a9fd95
Jason Miller <miller.jason.kyle@gmail.com>
2018-01-20 11:36:09 -0800

    Update - Download brew from master, update zipfile, and updated copytree function

--- macOS/brew_install.py
+++ macOS/brew_install.py
@@ -18,44 +18,43 @@ brew_temp_dir = tempfile.mkdtemp()
 #changing directory to new created temp directory
 os.chdir(brew_temp_dir)
 
-#downloading brew.sh from github version 1.4.3
-homebrew_url = urllib2.urlopen('https://github.com/Homebrew/brew/archive/1.4.3.zip')
-homebrew_zip = homebrew_url.read()
+#downloading brew.sh from github version the current version from Master
+homebrew_zip = urllib2.urlopen(
+    'https://github.com/Homebrew/brew/archive/master.zip').read()
 
 #Writing data to disk
 with open ("brew.zip", "w") as f:
     f.write(homebrew_zip)
 
-unzip = zipfile.ZipFile('brew.zip', "r")
-unzip.extractall(brew_temp_dir)
-unzip.close()
+with zipfile.ZipFile('brew.zip', 'r') as fobj:
+    fobj.extractall(brew_temp_dir)
 
 #Create Homebrew directory
-def createFolder('/usr/local/test/Homebrew')
+def createFolder(directory):
     try:
-        if not os.path.exists('/usr/local/test/Homebrew')
-            os.makedirs('/usr/local/test/Homebrew')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
     except OSError:
-        print ('Error: Creating directory.' + '/usr/local/test/Homebrew')
+        print ('Error: Creating directory.' + directory)
 
-createFolder('/usr/local/test/Homebrew')
+#createFolder('/Users/jasonmiller/Desktop/Homebrew')
 
-brew_home = ('/usr/local/test/Homebrew')
+brew_home = ('/Users/jasonmiller/Desktop/Homebrew/')
 
 def brew_copy(brew_temp_dir, brew_home):
     try:
-        shutil.copytree(src, dest)
+        shutil.copytree(brew_temp_dir, brew_home)
     except OSError as e:
         # If the error was caused because the source wasn't a directory
         if e.errno == errno.ENOTDIR:
-            shutil.copy(src, dest)
+            shutil.copy(brew_temp_dir, brew_home)
         else:
             print('Directory not copied. Error: %s' % e)
 
 brew_copy(brew_temp_dir, brew_home)
 
 #cleaning up
-#shutil.rmtree(brew_temp_dir)
+shutil.rmtree(brew_temp_dir)
 
 #used for testing
 #print brew_temp_dir
