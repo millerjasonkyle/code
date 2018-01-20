177627dbd7f666ca1e0ca99fe8034a15b6daeda1
Jason Miller <miller.jason.kyle@gmail.com>
2018-01-20 12:43:56 -0800

    testing this on 10.13

--- macOS/check_logged_in_user.py
+++ macOS/check_logged_in_user.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
-#import sys
-#from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+import sys
+from SystemConfiguration import SCDynamicStoreCopyConsoleUser
 
-#def logged_in_user():
-#	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
-#	username = [username,""][username in [u"loginwindow", None, u""]]
-#	return username
-
-#print logged_in_user()
+def logged_in_user():
+	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+	username = [username,""][username in [u"loginwindow", None, u""]]
+	return username
+print logged_in_user
 
 #import sys
 #from SystemConfiguration import SCDynamicStoreCopyConsoleUser
