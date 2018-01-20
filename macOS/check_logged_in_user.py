55b06a06042717b88efa64cfa4e169e59b3c36de
Jason Miller <miller.jason.kyle@gmail.com>
2018-01-20 12:30:14 -0800

    Adding gitignore file and checking logged in user

---
+++ macOS/check_logged_in_user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+#import sys
+#from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+
+#def logged_in_user():
+#	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+#	username = [username,""][username in [u"loginwindow", None, u""]]
+#	return username
+
+#print logged_in_user()
+
+#import sys
+#from SystemConfiguration import SCDynamicStoreCopyConsoleUser
+
+#def logged_in_user():
+#    username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0] 
+#    username = [username,""][username in [u"loginwindow", None, u""]]
+#    sys.stdout.write(username + "\n")
+
+#print logged_in_user
