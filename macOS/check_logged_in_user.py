f69c891791c0e1aa08c1258ba7a32df2c2d0dce4
Jason Miller <jasonmiller-sa@jasons-imac.lan>
2018-01-26 21:45:17 -0800

    update

--- macOS/check_logged_in_user.py
+++ macOS/check_logged_in_user.py
@@ -7,7 +7,7 @@ def logged_in_user():
 	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
 	username = [username,""][username in [u"loginwindow", None, u""]]
 	return username
-print logged_in_user
+print logged_in_user()
 
 #import sys
 #from SystemConfiguration import SCDynamicStoreCopyConsoleUser
