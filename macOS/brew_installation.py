e9fa810f4913b5fee84e3b734ab23bd4bf560959
Jason Miller <jasonmiller-sa@jasons-imac.lan>
2018-02-02 22:36:31 -0800

    Changes

--- macOS/brew_installation.py
+++ macOS/brew_installation.py
@@ -17,12 +17,18 @@ if not os.geteuid()==0:
     sys.exit("\nYou must be root to run this script, please use sudo and try again.\n") 
 
 #grabbed current logged in user in Apple approved way
-def logged_in_user():
-	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
-	username = [username,""][username in [u"loginwindow", None, u""]]
-	return username
-print logged_in_user()
+# def logged_in_user():
+# 	username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
+# 	username = [username,""][username in [u"loginwindow", None, u""]]
+# 	return username
+# print logged_in_user()
 
+def logged_in_user():
+    user = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])
+    user = [user,""][user[0] in [u"loginwindow", None, u""]]
+    return user
+
+username, uid, gid = logged_in_user
 
 #logged_in_user = pwd.getpwuid(os.getuid()).pw_name
 
